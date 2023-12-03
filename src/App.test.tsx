e7da7c2371c18cdd04e0e3bbefa12f0b125e3650
Frederico Alves <fredericoalves.info@gmail.com>
2023-12-03 23:58:49 +0100

    App.test updated

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Cypress Testing link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/More about Cypress Testing/);
   expect(linkElement).toBeInTheDocument();
 });
